8bafe29bfffc99e2dc9555f918396874cd08601c
briannageiger <briannageiger@oakland.edu>
2024-11-14 17:35:33 -0500

    in class assignment 2

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const picA = document.getElementById("A-pic");
+const picB = document.getElementById("B-pic");
+const picC = document.getElementById("C-pic");
+
+function clickBtnA() {
+    picA.style.visibility = "visible";
+}
+
+function clickBtnB() {
+    picB.style.visibility = "visible";
+}
+
+function clickBtnC() {
+    picC.style.visibility = "visible";
+}
+
+function hideA() {
+    picA.style.visibility = "hidden";
+}
+
+function hideB() {
+    picB.style.visibility = "hidden";
+}
+
+function hideC() {
+    picC.style.visibility = "hidden";
+}
